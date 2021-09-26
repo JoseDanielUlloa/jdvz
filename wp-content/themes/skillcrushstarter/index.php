f2f68e5750d7821cf29e5d344ef69e8786cd9cb4
JoseDanielMX <jduvmx@gmail.com>
2021-09-25 22:20:30 -0500

    Updated Skillcrush Starter Theme

--- wp-content/themes/skillcrushstarter/index.php
+++ wp-content/themes/skillcrushstarter/index.php
@@ -20,14 +20,26 @@ get_header(); ?>
 	<div class="main-content">
 		<?php if ( have_posts() ): ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-				<h3>Hello, Skillcrushers!</h3>
-				<p>This is your main index page.</p>
-				<p>The file that creates this default page is index.php.</p>
+				<?php get_template_part('content-blog', get_post_format()); ?>
 			<?php endwhile; ?>
 		<?php endif; ?>
 	</div>
 
 	<?php get_sidebar(); ?>
 </section>
+
+<nav id="navigation" class="container">
+	<div class="left">
+		<?php next_posts_link('&larr; <span>Older Posts</span>'); ?>
+	</div>
+	<div class="pagination">
+		<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+		echo 'Page '.$paged.' of '.$wp_query->max_num_pages;
+		?>
+	</div>
+	<div class="right">
+		<?php previous_posts_link('<span>Newer Posts</span> &rarr;'); ?>
+	</div>
+</nav>
 
 <?php get_footer(); ?>
