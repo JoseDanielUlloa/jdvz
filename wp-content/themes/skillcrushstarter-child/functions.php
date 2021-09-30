17f38b4ec1f5a0999a8e8a6b293898afce5d7fac
JoseDanielMX <jduvmx@gmail.com>
2021-09-29 20:54:42 -0500

    Added a child theme

---
+++ wp-content/themes/skillcrushstarter-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Skillcrush Starter Child functions and definitions
+*
+* @link http://codex.wordpress.org/Theme_Development
+* @link http://codex.wordpress.org/Child_Themes
+*
+* @package WordPress
+* @subpackage Skillcrush Starter
+* @since Skillcrush Starter 2.0
+*/
+
+// Enqueue scripts and styles
+function skillcrushstarter_child_scripts(){
+wp_enqueue_style( 'skillcrushstarter-style', get_template_directory_uri() . '/style.css' );
+wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'skillcrushstarter-style' ));
+}
+add_action( 'wp_enqueue_scripts', 'skillcrushstarter_child_scripts' );
