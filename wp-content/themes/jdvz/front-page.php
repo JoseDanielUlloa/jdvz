9a681bf461601a2d1453dde83fe02586bca6a5d8
JoseDanielMX <jduvmx@gmail.com>
2021-10-02 23:40:05 -0500

    Moved content to header.php and footer.php files

--- wp-content/themes/jdvz/front-page.php
+++ wp-content/themes/jdvz/front-page.php
@@ -1,20 +1,10 @@
 <?php
     get_header();
 ?>
-    <div class="main-wrapper">
-        <header class="page-title theme-bg-light text-center gradient py-5">
-            <h1 class="heading">Blog Home Page Heading</h1>
-        </header>
+
         <article class="content px-3 py-5 p-md-5">
             
         </article>
-        <footer class="footer text-center py-2 theme-bg-dark">
-
-            <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
-
-        </footer>
-
-    </div>
 
 <?php
     get_footer();
