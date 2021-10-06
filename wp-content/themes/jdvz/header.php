3916148bf6acdc36ac133ce9fce5c7ec34e67742
JoseDanielMX <jduvmx@gmail.com>
2021-10-05 20:20:05 -0500

    Updated favicon link

--- wp-content/themes/jdvz/header.php
+++ wp-content/themes/jdvz/header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">
-    <link rel="shortcut icon" href="/wp-content-themes/jdvz/assets/img/logo.png">
+    <link rel="shortcut icon" href="/wp-content-themes/jdvz/assets/img/favicon.png">
 
     <?php
     wp_head();
