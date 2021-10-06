dca4514b8db639e70025720b1c0d48855ad2767a
JoseDanielMX <jduvmx@gmail.com>
2021-10-05 20:51:37 -0500

    Deleted second jQuery call

--- wp-content/themes/jdvz/functions.php
+++ wp-content/themes/jdvz/functions.php
@@ -19,7 +19,6 @@ add_action( 'wp_enqueue_scripts', 'jdvz_register_styles');
 // Enqueue scripts
 function jdvz_register_scripts() {
     $version = wp_get_theme()->get( 'Version' );
-    wp_enqueue_script('jdvz-jquery', "https://code.jquery.com/jquery-3.6.0.slim.min.js", array(), '3.6.0', true);
     wp_enqueue_script('jdvz-popper', "https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js", array(), '2.9.3', true);
     wp_enqueue_script('jdvz-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js", array(), '5.1.1', true);
     wp_enqueue_script('jdvz-main', get_template_directory_uri() . "/assets/js/main.js", array(), $version, true);
