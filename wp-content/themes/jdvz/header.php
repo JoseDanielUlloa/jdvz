1527280532c58a6e18bf3b96902685ed9e525396
JoseDanielMX <jduvmx@gmail.com>
2021-10-05 20:46:13 -0500

    Removed favicon tag

--- wp-content/themes/jdvz/header.php
+++ wp-content/themes/jdvz/header.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">
-    <link rel="shortcut icon" href="/wp-content-themes/jdvz/assets/img/favicon.png">
 
     <?php
     wp_head();
