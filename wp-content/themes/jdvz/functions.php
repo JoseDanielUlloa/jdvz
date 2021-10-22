d007e6c43a053a7176bd1f593d35c0ec1bdf633b
JoseDanielMX <jduvmx@gmail.com>
2021-10-21 21:00:53 -0500

    Added a CSS reset

--- wp-content/themes/jdvz/functions.php
+++ wp-content/themes/jdvz/functions.php
@@ -10,7 +10,8 @@ add_action('after_setup_theme', 'jdvz_theme_support');
 // Enqueue styles
 function jdvz_register_styles() {
     $version = wp_get_theme()->get( 'Version' );
-    wp_enqueue_style('jdvz-stlye', get_template_directory_uri() . "/style.css", array(), $version, 'all');
+    wp_enqueue_style('jdvz-stlye', get_template_directory_uri() . "/style.css", array('jdvz-normalize'), $version, 'all');
+    wp_enqueue_style('jdvz-normalize', get_template_directory_uri() . "/assets/css/normalize.css", array(), '8.0.1', 'all');
     wp_enqueue_style('jdvz-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css", array(), '6.0.0', 'all');
 }
 add_action( 'wp_enqueue_scripts', 'jdvz_register_styles');
