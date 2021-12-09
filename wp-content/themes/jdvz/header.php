e07a08dd3a96ecc86ee46f659a00dfe64aa13591
JoseDanielMX <jduvmx@gmail.com>
2021-12-08 20:19:56 -0600

    Updated Contact page link

--- wp-content/themes/jdvz/header.php
+++ wp-content/themes/jdvz/header.php
@@ -90,7 +90,7 @@
                         </ul>
                     </li>
                     <li class="show-on-desktop">
-                        <a href="/contact/">
+                        <a href="<?php echo site_url('/contact/') ?>">
                             <i aria-hidden="true" class="far fa-envelope" title="Contact form"></i>
                             <span class="sr-only">Contact form</span>
                         </a>
