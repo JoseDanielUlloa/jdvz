c6dc1ab5560ad86a0510d3cdbc0fca8faf598765
JoseDanielMX <jduvmx@gmail.com>
2021-12-23 21:06:24 -0600

    Changed the way that post meta data will be displayed

--- wp-content/themes/jdvz/template-parts/content-article.php
+++ wp-content/themes/jdvz/template-parts/content-article.php
@@ -1,18 +1,17 @@
 <article>
     <!-- Post thumbnail -->
     <?php if ( has_post_thumbnail() ) : ?>
-    <figure>
-        <?php the_post_thumbnail('full'); ?>
-    </figure>
+        <figure>
+            <?php the_post_thumbnail('full'); ?>
+        </figure>
     <?php endif; ?>
 
     <!-- Post meta data -->
     <h2><?php the_title(); ?></h2>
-    <i class="fas fa-user-edit" aria-hidden="true"></i> Written by <?php the_author_posts_link(); ?>
-    <i class="fas fa-list-ul" aria-hidden="true"></i> Posted in <?php the_category(' '); ?>
-    <i class="far fa-calendar-alt" aria-hidden="true"></i> Published on <?php the_date(); ?>
-    <i class="fa fa-tag" aria-hidden="true"></i> Tags: <?php the_tags('<span class="tag">', '</span> <span class="tag">', '</span>'); ?>
-    <i class="fa fa-comment" aria-hidden="true"></i> <a href="#comments"><?php comments_number(); ?></a>
+    Posted by <?php the_author_posts_link(); ?>
+    on <?php the_date(); ?>
+    in <?php the_category(' '); ?>
+    with the tags <?php the_tags('<span class="tag">', '</span> <span class="tag">', '</span>.'); ?>
 
     <!-- Content -->
     <?php the_content(); ?>
