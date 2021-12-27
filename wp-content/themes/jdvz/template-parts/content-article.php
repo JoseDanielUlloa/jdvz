02885baea2e1d28db1d5203bea53c250cda13eb0
JoseDanielMX <jduvmx@gmail.com>
2021-12-26 22:31:13 -0600

    Added previous and next post links

--- wp-content/themes/jdvz/template-parts/content-article.php
+++ wp-content/themes/jdvz/template-parts/content-article.php
@@ -16,6 +16,19 @@
     <!-- Content -->
     <?php the_content(); ?>
 
+    <!-- Previous & Next posts -->
+    <nav id="posts-nav" class="posts-nav" aria-label="Posts navigation">
+        <h2 id="prev-next-posts" class="sr-only">Posts navigation</h2>
+        <ul aria-labelledby="prev-next-posts" class="list-reset">
+            <li>
+                <?php previous_post_link('%link', '<i class="fas fa-arrow-left" aria-hidden="true"></i> Previous post'); ?>
+            </li>
+            <li>
+                <?php next_post_link('%link', 'Next post <i class="fas fa-arrow-right" aria-hidden="true"></i>'); ?>
+            </li>
+        </ul>
+    </nav>
+
     <!-- Comments -->
     <?php comments_template(); ?>
 </article>
