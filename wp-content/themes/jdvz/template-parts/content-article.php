a1409c6331f9040ee4224345fd4a1febdd2eaf20
JoseDanielMX <jduvmx@gmail.com>
2021-12-27 19:35:46 -0600

    Styled post meta data

--- wp-content/themes/jdvz/template-parts/content-article.php
+++ wp-content/themes/jdvz/template-parts/content-article.php
@@ -7,12 +7,14 @@
     <?php endif; ?>
 
     <!-- Post meta data -->
-    <h2><?php the_title(); ?></h2>
-    Posted by <?php the_author_posts_link(); ?>
-    on <?php the_date(); ?>
-    in <?php the_category(' '); ?>
-    with the tags <?php the_tags('<span class="tag">', '</span> <span class="tag">', '</span>.'); ?>
-
+    <h1><?php the_title(); ?></h1>
+    <p class="post-meta-data">
+        Posted by <?php the_author_posts_link(); ?>
+        on <?php the_date(); ?>
+        in <?php the_category(' '); ?>
+        with the tags <?php the_tags('', ' ', '.'); ?>
+    </p>
+    
     <!-- Content -->
     <?php the_content(); ?>
 
